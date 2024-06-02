616aee704dfc3f15b72e886bb5b974089a03bb74
nelvson <nelvson1337@gmail.com>
2024-06-02 08:56:19 +0700

    parse XML. fix y_tile, use @log instead of math.log()

---
+++ script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+read_dom () {
+    local IFS=\>
+    read -d \< ENTITY CONTENT
+    local RET=$?
+    TAG_NAME=${ENTITY%% *}
+    ATTRIBUTES=${ENTITY#* }
+    return $RET
+}
+
+parse_dom () {
+    if [[ $TAG_NAME = "trkpt" ]] ; then
+        eval local $ATTRIBUTES
+        echo "$lat, $lon"
+    fi
+}
+
+while read_dom; do
+    parse_dom
+done
--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,18 @@
 const std = @import("std");
 
+const print = std.debug.print;
+const math = std.math;
+const PI = math.pi;
+
 pub fn get_tiles(lat: f64, long: f64, zoom: u5) struct { x_tile: i32, y_tile: i32 } {
     const n: f64 = @floatFromInt(@as(i32, 1) << zoom);
-    const x_tile: i32 = @intFromFloat(std.math.floor(n * (long + 180.0) / 360.0));
+    const x_tile: i32 = @intFromFloat(math.floor(n * (long + 180.0) / 360.0));
 
-    const lat_rad = std.math.degreesToRadians((lat));
-    const y_tile: i32 = @intFromFloat(std.math.floor(n *
-        (1.0 - std.math.log(f64, std.math.tan(lat_rad) + (1.0 / std.math.cos(lat_rad)), 10) / std.math.pi) / 2.0));
+    const lat_rad: f64 = math.degreesToRadians((lat));
+    const log = @log((math.tan(lat_rad) + (1.0 / math.cos(lat_rad))));
+
+    const y_tile: i32 = @intFromFloat(math.floor(n *
+        (1.0 - log / PI) / 2.0));
     return .{ .x_tile = x_tile, .y_tile = y_tile };
 }
 
@@ -16,13 +22,13 @@ pub fn main() !void {
     // stdout, not any debugging messages.
     //    <trkpt lat="-6.2432460" lon="106.6315630">
 
-    const lat: f64 = -6.2432460;
-    const lon: f64 = 106.6315630;
+    const lat: f64 = -6.2498060;
+    const lon: f64 = 106.6258030;
     // get_tiles is not fully working (yet). returned values are wrong
     const tiles = get_tiles(
         lat,
         lon,
         18,
     );
-    std.debug.print("{d}--{d}", .{ tiles.x_tile, tiles.y_tile });
+    print("{d}--{d}", .{ tiles.x_tile, tiles.y_tile });
 }
